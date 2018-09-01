6c268f9aeca56054ed7ab33d8380ccdc3accac58
Ellie <elliewem@gmail.com>
2018-09-01 20:35:12 +0100

    Configure django and wagtail

--- backend/settings.py
+++ backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vteo7uvs6cqnye9hqsp(!h&sc$jyw$k8s)frdhb)6@(s*r&a!-'
+SECRET_KEY = SUPER_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
