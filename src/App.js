1a69212202b63e61fd67604aaad553f6bbde0998
Ellie <elliewem@gmail.com>
2018-09-03 10:38:04 +0100

    Delete presets

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      siteData: null
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("/api/site/")
+      .then(response => {
+        this.setState({ siteData: response.data });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{this.state.siteData}</p>
       </div>
     );
   }
