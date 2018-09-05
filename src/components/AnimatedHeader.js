5a83397106c965ea4a2aa87cdfe9e8059983f927
Ellie <elliewem@gmail.com>
2018-09-05 14:48:29 +0100

    Add animation to svg

--- src/components/AnimatedHeader.js
+++ src/components/AnimatedHeader.js
@@ -1,13 +1,29 @@
 import React, { Component } from "react";
+import { TimelineLite } from "gsap/all";
 
 class AnimatedHeader extends Component {
+  componentDidMount() {
+    const timeline0 = new TimelineLite();
+    timeline0.from(this.refs.container0, 2, { width: 0 });
+    timeline0.from(this.refs.word0, 2.5, { opacity: 0, top: "100px" });
+
+    const timeline1 = new TimelineLite();
+    timeline1.from(this.refs.word1, 2.5, { opacity: 0, bottom: "100px" }, 2.4);
+  }
+
   splitChars() {
     const { heading } = this.props;
     const split = heading.split(" ").map((word, index) => {
       return (
-        <h1 key={index} className="animated-header__text">
-          {word.toUpperCase()}
-        </h1>
+        <div
+          key={index}
+          ref={`container${index}`}
+          className="animated-header__text-wrapper"
+        >
+          <h1 ref={`word${index}`} className="animated-header__text">
+            {word.toUpperCase()}
+          </h1>
+        </div>
       );
     });
     return split;
--- src/components/HomeBackgroundImage.js
+++ src/components/HomeBackgroundImage.js
@@ -1,16 +1,12 @@
 import React, { Component } from "react";
-import { drawSVG, TimelineLite } from "gsap/all";
+import { TimelineLite } from "gsap/all";
+import DrawSVGPlugin from "../plugins/DrawSVGPlugin";
 import InlineSVG from "svg-inline-react";
 
 class HomeBackgroundImage extends Component {
   componentDidMount() {
-    TweenLite.to("#path-1", 1, { drawSVG: "20% 80%" });
-    TweenLite.to("#path-2", 1, { drawSVG: "20% 80%" });
-    TweenLite.to("#path-3", 1, { drawSVG: "20% 80%" });
-    TweenLite.to("#path-4", 1, { drawSVG: "20% 80%" });
-    TweenLite.to("#path-5", 1, { drawSVG: "20% 80%" });
-    TweenLite.to("#path-6", 1, { drawSVG: "20% 80%" });
-    TweenLite.to("#path-7", 1, { drawSVG: "20% 80%" });
+    const timeline = new TimelineLite();
+    timeline.from(".cls-1", 10, { drawSVG: 0 });
   }
 
   render() {
